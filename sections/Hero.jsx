6cbc05434b09db0dbd0806ab8842a685a3e704f3
gopalverma1303 <vgopaldps@gmail.com>
2023-10-20 13:20:00 +0530

    Added title neon glow

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -15,23 +15,15 @@ const Hero = () => (
       className={`${styles.innerWidth} mx-auto flex flex-col relative w-full h-[550px]`}
     >
       <div className="flex justify-center items-center flex-col relative z-10">
-        <motion.h1 variants={textVariant(1.1)} className={`${styles.heroHeading}`}>
+        <motion.h1 variants={textVariant(1.1)} className={`${styles.heroHeading} neon-glow`}>
           Code
         </motion.h1>
-        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+        <motion.h1 variants={textVariant(1.1)} className={`${styles.heroHeading} neon-glow`}>
           Deploying
         </motion.h1>
-        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+        <motion.h1 variants={textVariant(1.1)} className={`${styles.heroHeading} neon-glow`}>
           Squad
         </motion.h1>
-        {/* <motion.div
-          variants={textVariant(1.2)}
-          className="flex flex-row justify-center items-center"
-        >
-          <h1 className={styles.heroHeading}>Ma</h1>
-          <div className={styles.heroDText} />
-          <h1 className={styles.heroHeading}>Ness</h1>
-        </motion.div> */}
       </div>
 
       {/* <motion.div
